da307d07d0b2a2e8ca426486341a7a310e13dabc
xshen <mshen_85@hotmail.com>
2018-12-28 12:57:08 +0800

    d

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -1,14 +1,23 @@
 import { VoterService } from "./voter.service";
+import { ISession } from "../index";
+import { of } from "rxjs";
 
 
 describe('VoterService',()=>{
-    let voterService: VoterService;
+    let voterService: VoterService,
     mockHttp;
 
-    beforeEach() =>{
-        mockHttp = jasmine.createSpyObj('mockHttp',)
+    beforeEach(() =>{
+        mockHttp = jasmine.createSpyObj('mockHttp',['delete','post'])
         voterService = new VoterService(mockHttp);
-
-    }
+    });
     
+    describe('deleteVoter',()=>{
+        it('should remove the voter from the list of voters',()=>{
+            var session = {id:6, voters:["joe","john"]};
+            mockHttp.delete.and.returnValue(of(false));
+            voterService.deleteVoter(3,<ISession>session,"joe");
+            expect(session.voters.length).toBe(1);
+        })
+    })
 })
